6d3803849a0b86fab867c7e47427a2816308dd0b
Abdale Hassan <cabdale989@gmail.com>
2021-05-19 17:59:41 -0500

    complete Employee class and Employee testing

--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -2,8 +2,15 @@ const fs = require('fs');
 const Employee = require('../lib/Employee');
 
 it("it should return name, id, and email", () =>{
-    const employee = new Employee ('Abdale', 4, 'cabdale989@gmail.com');
-    expect(employee.name).toEqual(expect.any(String));
-    expect(employee.id).toEqual(expect.any(Number));
-    expect(employee.Email).toEqual(expect.any(String));
+    const employee = new Employee ('James', 4, 'cabdale989@gmail.com');
+    expect(employee.getName()).toEqual('James');
+})
+it("it should return name, id, and email", () =>{
+    const employee = new Employee ('James', 4, 'cabdale989@gmail.com');
+    expect(employee.getId()).toEqual(4);
+ 
+})
+it("it should return name, id, and email", () =>{
+    const employee = new Employee ('James', 4, 'cabdale989@gmail.com');
+    expect(employee.getEmail()).toEqual('cabdale989@gmail.com');
 })
