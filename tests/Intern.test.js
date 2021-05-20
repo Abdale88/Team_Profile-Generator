1bdddc4080b8220f12ded6a2d99fe23ff1d0735c
Abdale Hassan <cabdale989@gmail.com>
2021-05-19 21:53:41 -0500

    complete intern class and run the test

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -1,4 +1,23 @@
-Employee.test.js
-  Engineer.test.js
-  Intern.test.js
-  Manager.test.js
+const Intern = require('../lib/Intern');
+
+describe('Engineer', () => {
+    it('should return the name of the intern', () => {
+        const name = new Intern('Abdale', 5, 'cabdale989@gmail.com', 'KU')
+        expect(name.getName()).toEqual('Abdale');
+    })
+
+    it('should return the id of the intern', () => {
+        const id = new Intern('Abdale', 5, 'cabdale989@gmail.com', 'KU')
+        expect(id.getId()).toEqual(5);
+    })
+
+    it('should return the email of the intern', () => {
+        const email = new Intern('Abdale', 5, 'cabdale989@gmail.com', 'KU')
+        expect(email.getEmail()).toEqual('cabdale989@gmail.com')
+    })
+
+    it('should return the school of the intern', () => {
+        const school = new Intern('Abdale', 5, 'cabdale989@gmail.com', 'KU')
+        expect(school.getSchool()).toEqual('KU')
+    })
+})
