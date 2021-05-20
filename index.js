22f85df5efc530bfca04ae559aeb4a3f6fafd3b3
Abdale Hassan <cabdale989@gmail.com>
2021-05-20 11:04:59 -0500

    requiring classes

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const fs = require('fs');
 const Employee = require('./lib/Employee');
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+const fs = require('fs');
 
 
 const employee = new Employee();
